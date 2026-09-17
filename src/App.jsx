6725a78f9ccf870f9bbcd4274f2bec6c5169bdeb
Alal Hosen <alalhosen1992620120@gmail.com>
2026-09-17 20:19:20 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+     <h2 className='text-4xl font-bold flex items-center justify-center min-h-screen'>Movie Explorer</h2>
+    </>
+  )
+}
+
+export default App
